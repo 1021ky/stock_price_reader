066da99adf52ef46a327026fb6836f52fb4b8413
k_yamanaka <vaivailx+git@gmail.com>
2021-07-14 22:33:13 +0900

    mockを使ったテストを追加する
    まだうまくmockは動いてないようだけど一度コミット

--- app/stock_info_accessor.py
+++ app/stock_info_accessor.py
@@ -8,13 +8,6 @@ import requests
 from pandas import DataFrame
 from requests.exceptions import Timeout
 
-# APIにリクエストを投げる間隔（sec）
-REQUEST_INTERVAL = 2
-# APIに繋がらないときのタイムアウト
-REQUEST_TIMEOUT = 5.0
-# APIにリクエストしてタイムアウトしたときのリトライ数
-RETRY_COUNT = 3
-
 
 class YahooFinanceAPIConfigure:
     """YahooFinanceAPIへの設定"""
@@ -47,51 +40,74 @@ class YahooFinanceAPIConfigure:
         )
 
 
-_yahoo_finance_api_configure = YahooFinanceAPIConfigure("6months", "1day")
+class YahooFinanceAPIClient:
+    # APIにリクエストを投げる間隔（sec）
+    _REQUEST_INTERVAL = 2
+    # APIに繋がらないときのタイムアウト
+    _REQUEST_TIMEOUT = 5.0
+    # APIにリクエストしてタイムアウトしたときのリトライ数
+    _RETRY_COUNT = 3
 
+    def __init__(self) -> None:
+        self._configure = YahooFinanceAPIConfigure("6months", "1day")
 
-def get_stockinfo(stock_code: str) -> Optional[DataFrame]:
-    """APIから情報取得する"""
-    url = _yahoo_finance_api_configure.generate_url(stock_code)
-    logging.debug("url:%s", url)
-    text = {}
-    for _ in range(RETRY_COUNT):
+    def get_stockinfo(self, stock_code: str) -> Optional[DataFrame]:
+        """APIから銘柄情報を取得する"""
+        url = self._configure.generate_url(stock_code)
+        logging.info("url:%s", url)
+        response = self._request(url)
+        logging.debug("response:%s", response)
+        parsed = self._parse_response(response)
+        logging.debug("parsed response:%s", parsed)
+        return parsed
+
+    def _request(self, url: str) -> Optional[Dict]:
+        headers = {
+            "User-Agent": "Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/51.0.2704.103 Safari/537.36"
+        }
+
+        for _ in range(self._RETRY_COUNT):
+            try:
+                res = requests.get(url, timeout=self._REQUEST_TIMEOUT, headers=headers)
+                sleep(self._REQUEST_INTERVAL)
+                if res.status_code == requests.codes.ok:
+                    jsondata = res.json()
+                    return jsondata
+            except Timeout as e:
+                logging.debug(e)
+                sleep(self._REQUEST_INTERVAL)
+                continue
+            except Exception as e:
+                logging.debug(e)
+                continue
+        else:
+            logging.warn("timeout occurred. url is %s", url)
+            return None
+
+    def _parse_response(self, response: Optional[Dict]) -> Optional[DataFrame]:
+        if response is None:
+            return None
+        if response["chart"]["error"] is not None:
+            logging.warn("api error %s", response["chart"]["error"])
+            return None
         try:
-            res = requests.get(url, timeout=REQUEST_TIMEOUT)
-            text = json.load(StringIO(res.text))
-        except Timeout:
-            sleep(REQUEST_INTERVAL)
-            continue
-        except Exception:
-            continue
-        break
-    else:
-        logging.warn("timeout occurred. code is %s", stock_code)
-        return None
-    if text["chart"]["error"] is not None:
-        logging.warn("api return error. code is %s", stock_code)
-        logging.warn("api error %s", text["chart"]["error"])
-        return None
-    sleep(REQUEST_INTERVAL)
-    try:
-        return _parse_json_to_dataframe(text)
-    except TypeError as e:
-        logging.warn("unexpected format data returned.")
-        logging.warn(e)
-        return None
+            return self._parse_json_to_dataframe(response)
+        except TypeError as e:
+            logging.warn("unexpected format data returned.")
+            logging.warn(e)
+            return None
 
-
-def _parse_json_to_dataframe(json_data: Dict) -> DataFrame:
-    try:
-        d = json_data["chart"]["result"][0]
-        df = DataFrame()
-        df["timestamp"] = d["timestamp"]
-        df["open"] = d["indicators"]["quote"][0]["open"]
-        df["low"] = d["indicators"]["quote"][0]["low"]
-        df["high"] = d["indicators"]["quote"][0]["high"]
-        df["close"] = d["indicators"]["quote"][0]["close"]
-        df["volume"] = d["indicators"]["quote"][0]["volume"]
-    except TypeError as e:
-        logging.warn("failed to parse json")
-        raise e
-    return df
+    def _parse_json_to_dataframe(self, json_data: Dict) -> DataFrame:
+        try:
+            d = json_data["chart"]["result"][0]
+            df = DataFrame()
+            df["timestamp"] = d["timestamp"]
+            df["open"] = d["indicators"]["quote"][0]["open"]
+            df["low"] = d["indicators"]["quote"][0]["low"]
+            df["high"] = d["indicators"]["quote"][0]["high"]
+            df["close"] = d["indicators"]["quote"][0]["close"]
+            df["volume"] = d["indicators"]["quote"][0]["volume"]
+        except TypeError as e:
+            logging.warn("failed to parse json")
+            raise e
+        return df
