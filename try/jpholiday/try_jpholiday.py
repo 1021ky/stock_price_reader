7a8b707c241dae9ab0d8ab530061e9a7d27d24b6
k_yamanaka <vaivailx+git@gmail.com>
2021-07-23 08:43:59 +0900

    jpholidayで試すことを書き出す

---
+++ try/jpholiday/try_jpholiday.py
@@ -0,0 +1,6 @@
+"""jpholidayのお試し記録
+やること
+* jpholidayで祝日を出す
+* カスタムの休日を出せるようにする
+* pandas.datatimeindex(https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DatetimeIndex.html)に変換できるようにする
+"""
