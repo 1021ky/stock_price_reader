f63c73b9363c428394fbef7a7c593c7610ff8e22
k_yamanaka <vaivailx+git@gmail.com>
2021-07-24 12:22:21 +0900

    jpholidayの値をDatetimeIndexに変換するお試しをする

--- try/jpholiday/try_jpholiday.py
+++ try/jpholiday/try_jpholiday.py
@@ -5,14 +5,14 @@
 * pandas.datatimeindex(https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DatetimeIndex.html)に変換できるようにする
 """
 import jpholiday
-import datetime
+from datetime import date, timedelta, timezone
 
-from numpy import is_busday
+from pandas import DatetimeIndex
 
 # jpholidayで休日を出す
 def holidays():
     # 指定範囲の祝日を取得
-    result = jpholiday.between(datetime.date(2021, 7, 1), datetime.date(2021, 10, 1))
+    result = jpholiday.between(date(2021, 7, 1), date(2021, 10, 1))
     print("------result------")
     print(result)
     print(type(result))
@@ -60,9 +60,46 @@ def customed_holidays():
         print(f"{y}年の休日は{len(results)}間")
 
 
+def jpholidays_to_datetimeindex():
+    class TSEHoliday(jpholiday.OriginalHoliday):
+        def _is_holiday(self, date):
+            # 年末年始は休み
+            month = date.month
+            day = date.day
+            if month == 12 and day in [31]:
+                return True
+            elif month == 1 and day in [1, 2, 3]:
+                return True
+            # 土日は休み
+            if date.weekday() >= 5:
+                return True
+            return False
+
+        def _is_holiday_name(self, date):
+            # 年末年始は休み
+            month = date.month
+            day = date.day
+            if month == 12 and day in [31]:
+                return "年末年始休暇"
+            elif month == 1 and day in [1, 2, 3]:
+                return "年末年始休暇"
+            # 土日は休み
+            if date.weekday() == 5:
+                return "土曜日"
+            if date.weekday() == 6:
+                return "日曜日"
+
+    results = jpholiday.year_holidays(2021)
+    JST = timezone(timedelta(hours=+9), "JST")
+
+    index = DatetimeIndex([day for day, _ in results], tz=JST)
+    print(index)
+
+
 def main():
     # holidays()
-    customed_holidays()
+    # customed_holidays()
+    jpholidays_to_datetimeindex()
 
 
 if __name__ == "__main__":
