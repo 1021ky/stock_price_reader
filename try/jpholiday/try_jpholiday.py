2c4c33ceb10f96e621c5f9a7c99f2e931bb58ff2
k_yamanaka <vaivailx+git@gmail.com>
2021-07-24 12:10:35 +0900

    jpholidayで独自の休日を設定して判定するのをためした

--- try/jpholiday/try_jpholiday.py
+++ try/jpholiday/try_jpholiday.py
@@ -1,6 +1,69 @@
 """jpholidayのお試し記録
 やること
-* jpholidayで祝日を出す
+* jpholidayで休日を出す
 * カスタムの休日を出せるようにする
 * pandas.datatimeindex(https://pandas.pydata.org/pandas-docs/stable/reference/api/pandas.DatetimeIndex.html)に変換できるようにする
 """
+import jpholiday
+import datetime
+
+from numpy import is_busday
+
+# jpholidayで休日を出す
+def holidays():
+    # 指定範囲の祝日を取得
+    result = jpholiday.between(datetime.date(2021, 7, 1), datetime.date(2021, 10, 1))
+    print("------result------")
+    print(result)
+    print(type(result))
+    for r in result:
+        print(type(r))
+        print(r)
+
+
+def customed_holidays():
+    # 東証の休日を追加
+    class TSEHoliday(jpholiday.OriginalHoliday):
+        def _is_holiday(self, date):
+            # 年末年始は休み
+            month = date.month
+            day = date.day
+            if month == 12 and day in [31]:
+                return True
+            elif month == 1 and day in [1, 2, 3]:
+                return True
+            # 土日は休み
+            if date.weekday() >= 5:
+                return True
+            return False
+
+        def _is_holiday_name(self, date):
+            # 年末年始は休み
+            month = date.month
+            day = date.day
+            if month == 12 and day in [31]:
+                return "年末年始休暇"
+            elif month == 1 and day in [1, 2, 3]:
+                return "年末年始休暇"
+            # 土日は休み
+            if date.weekday() == 5:
+                return "土曜日"
+            if date.weekday() == 6:
+                return "日曜日"
+
+    results = jpholiday.year_holidays(2021)
+    print(results)
+    print(f"2021年の休日は{len(results)}間")
+
+    for y in range(2000, 2030):
+        results = jpholiday.year_holidays(y)
+        print(f"{y}年の休日は{len(results)}間")
+
+
+def main():
+    # holidays()
+    customed_holidays()
+
+
+if __name__ == "__main__":
+    main()
